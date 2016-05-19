acc5132a3616932485e0d455420985c40d7ce582
Raim Interiano <Raimmaster@users.noreply.github.com>
2016-05-19 11:46:58 -0600

    Assets for ships, lasers, and buttons; CrossPlatformInput used for moving the ship

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerController : MonoBehaviour {
+
+	Rigidbody2D playerBody;
+	public float speedForce = 5f;
+	public float increaseBoost = 2f;
+	public float defaultBoost = 1f;
+	
+	// Use this for initialization
+	void Start () {
+		playerBody = this.GetComponent<Rigidbody2D>();
+	}
+	
+	//FixedUpdate por usar vectores, fuerzas
+	void FixedUpdate () {
+		Vector2 moveVector = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal"),
+			CrossPlatformInputManager.GetAxis("Vertical")) * speedForce;
+
+		bool isBoost = CrossPlatformInputManager.GetButton("Boost");
+		Debug.Log(isBoost);
+		playerBody.AddForce(moveVector * (isBoost ? increaseBoost : defaultBoost));
+	}
+}
