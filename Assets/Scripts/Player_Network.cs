203c7be64a34fdc7d721f162a895a20c1c077c1b
Luis Carlos Isaula <luis_isaula@hotmail.com>
2016-05-22 23:33:46 -0600

    Pruebas LAN Networking Manager

---
+++ Assets/Scripts/Player_Network.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Player_Network : NetworkBehaviour {
+	public float movementLerpRate = 15;
+	[SyncVar] private Vector2 syncPosition;
+
+	private Transform playerTransform;
+	void Start () {
+		playerTransform = GetComponent<Transform>();
+		if(!isLocalPlayer){
+			GetComponent<PlayerController>().enabled = false;
+		}
+	}
+
+	private void FixedUpdate(){
+		LerpPosition();
+		Sendpostion();
+	}
+
+	private void LerpPosition()
+	{	
+		if(!isLocalPlayer){
+			playerTransform.position = Vector2.Lerp(playerTransform.position, syncPosition, Time.deltaTime * movementLerpRate);
+		}
+
+	}
+
+	[Command]
+	private void CmdProvidePosToServer(Vector2 position){
+		syncPosition = position;
+	}
+
+	[ClientCallback]
+	private void Sendpostion(){
+		if(isLocalPlayer)
+			CmdProvidePosToServer(playerTransform.position);
+	}
+	
+}
